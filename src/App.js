088718e3e6dc7b98fa92e2940904f19f86403d3d
Rafa Tárrega <rafatarrega@cecotec.es>
2019-04-26 12:32:28 +0200

    empezando el proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './componentes/Header';
+import AgregarCita from './componentes/AgregarCita';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header 
+        titulo = {'Administrador de Pacientes de Veterinaria'}
+      />
+      <div className="row">
+        <div className="col-md-6">
+          <AgregarCita 
+
+          />
+        </div>
+      </div>
     </div>
   );
 }
